af02f5acb95777636c4d1de1c80eb98e8583028d
Le Phan <lvphan@outlook.com>
2019-05-24 14:40:28 -0500

    Updated main.py/game.py and added inventory.py
    main.py
    Added Items
    Added comments to specify certain sections of code
    Removed code that isn't being utilized
    Moved Spells array for easier referencing
    Item quantity was fixed (a string) but changed so quantity would not be fixed
    Added a print statement for when an item is selected to be used but the quantity is 0
    Added a note to type 0 to go back a menu when either selecting a spell or item
    Changed bad number message for choosing a number that isn't (1-3) since Items is now #3
    
    inventory.py
    Created Item class
    
    game.py
    Added choose item method
    Adjusted the text for a better aesthetic

--- classes/game.py
+++ classes/game.py
@@ -1,6 +1,4 @@
 import random
-import pprint
-from .magic import Spell
 
 
 class bcolors:
@@ -14,20 +12,17 @@ class bcolors:
     UNDERLINE = '\033[4m'
 
 class Player:
-    def __init__(self, hp, mp, atk, df, magic):
+    def __init__(self, hp, mp, atk, df, magic, items):
         self.max_hp = hp
         self.hp = hp
-
         self.max_mp = mp
         self.mp = mp
-
         self.atk_low = atk - 10
         self.atk_high = atk + 10
-
         self.df = df
         self.magic = magic
-
-        self.actions = ["Attack", "Magic"]
+        self.items = items
+        self.actions = ["Attack", "Use Magic", "Use Item(s)"]
 
     def generate_damage(self):
         return random.randrange(self.atk_low, self.atk_high)
@@ -60,14 +55,21 @@ class Player:
 
     def choose_action(self):
         i = 1
-        print(bcolors.OKBLUE + bcolors.BOLD + "Actions" + bcolors.ENDC)
+        print("\n" + bcolors.OKBLUE + bcolors.BOLD + "ACTIONS:" + bcolors.ENDC)
         for item in self.actions:
-            print(str(i) + ":", item)
+            print("    " + str(i) + ".", item)
             i += 1
 
     def choose_spell(self):
         i = 1
-        print(bcolors.OKBLUE + bcolors.BOLD + "Spells" + bcolors.ENDC)
+        print("\n" + bcolors.OKBLUE + bcolors.BOLD + "SPELLS:" + bcolors.ENDC)
         for spell in self.magic:
-            print(str(i) + ":", spell.name, "(Cost:", str(spell.cost) + ")")
+            print("    " + str(i) + ".", spell.name, "(Cost:", str(spell.cost) + ")")
+            i += 1
+
+    def choose_item(self):
+        i = 1
+        print("\n" + bcolors.OKBLUE + bcolors.BOLD + "ITEMS:" + bcolors.ENDC)
+        for item in self.items:
+            print("    " + str(i) + ".", item["Item"].name + ":", item["Item"].description, "(x" + str(item["Quantity"]) + ")")
             i += 1
--- main.py
+++ main.py
@@ -1,31 +1,42 @@
 from classes.game import Player, bcolors
 from classes.magic import Spell
+from classes.inventory import Item
 
 
+###Spells###
 #Dark Spells
 meteor = Spell("Meteor Strike", 20, 50, "Dark")
 typhoon = Spell("Typhoon Strike", 25, 55, "Dark")
 lighting = Spell("Lightning Strike", 50, 70, "Dark")
 ultra = Spell("PLUS ULTRA", 100, 100, "Dark")
-
-
 #Light Spells
 cure = Spell("Cure", 10, 15, "Light")
 cure_p = Spell("Cure +", 15, 20, "Light")
 
+player1_spells = [meteor, typhoon, lighting, ultra, cure, cure_p]
 
-'''
-Using a Dictionary to create and access spells
-magic = [{"Name": "Meteor Strike", "Cost": 20, "Damage": 50},
-         {"Name": "Typhoon Strike", "Cost": 25, "Damage": 55},
-         {"Name": "Lightning Strike", "Cost": 50, "Damage": 70},
-         {"Name": "PLUS ULTRA", "Cost": 100, "Damage": 100}]
-'''
+###Items###
+#Health Potions
+red_potion = Item("Red Potion", "Potion", "Heals 10 HP", 10)
+orange_potion = Item("Orange Potion", "Potion", "Heals 25 HP", 25)
+white_potion = Item("White Potion", "Potion", "Heals 50 HP", 50)
+#Mana Potions
+blue_potion = Item("Blue Potion", "Potion", "Heals 10 MP", 10)
+big_blue_potion = Item("Big Blue Potion", "Potion", "Heals 50 MP", 50)
+#Elixirs
+purple_elixir = Item("Purple Elixir", "Elixir", "Fully restores HP/MP of one party member", 999)
+big_purple_elixir = Item("Big Purple Elixir", "Elixir", "Fully restores parties HP/MP", 999)
+#Throwing Stars
+shuriken = Item("Shuriken", "Throwing Star", "Throw a shuriken dealing 80 damage", 80)
 
-
+player1_items = [{"Item": red_potion, "Quantity": 10},{"Item": orange_potion, "Quantity": 5},
+                 {"Item": white_potion, "Quantity": 5},{"Item": blue_potion, "Quantity": 5},
+                 {"Item": big_blue_potion, "Quantity": 5},{"Item": purple_elixir, "Quantity": 1},
+                 {"Item": big_purple_elixir, "Quantity": 1},{"Item": shuriken, "Quantity": 3}]
+###Player & Enemy###
 #Instantiate Player & Enemy
-player1 = Player(100, 100, 50, 50, [meteor, typhoon, lighting, ultra, cure, cure_p])
-enemy = Player(300, 100, 30, 25, [])
+player1 = Player(100, 100, 50, 50, player1_spells, player1_items)
+enemy = Player(300, 100, 30, 25, [], [])
 
 running = True
 
@@ -34,7 +45,7 @@ print(bcolors.FAIL + bcolors.BOLD + "AN ENEMY APPEARS!" + bcolors.ENDC)
 while running:
     print("=========================")
     player1.choose_action()
-    choice = input("Choose Action:")
+    choice = input("Choose Action: ")
     index = int(choice) - 1
 
     if index == 0:
@@ -44,7 +55,11 @@ while running:
     elif index == 1:
         print("-------------------------")
         player1.choose_spell()
+        print("    " + bcolors.FAIL + "0. Go Back" + bcolors.ENDC)
         spell_choice = int(input("Choose Spell:")) - 1
+
+        if spell_choice == -1:
+            continue
 
         spell = player1.magic[spell_choice]
         magic_dmg = spell.generate_damage()
@@ -66,13 +81,38 @@ while running:
         elif spell.type == "Dark":
             enemy.take_damage(magic_dmg)
             print(bcolors.OKBLUE + "\n" + spell.name + " deals", str(magic_dmg), "points of damage." + bcolors.ENDC)
-    elif index != 0 and index != 1:
+    elif index == 2:
+        print("-------------------------")
+        player1.choose_item()
+        print("    " + bcolors.FAIL + "0. Go Back" + bcolors.ENDC)
+        item_choice = int(input("Choose Item: ")) - 1
+
+        item = player1.items[item_choice]["Item"]
+
+        if player1.items[item_choice]["Quantity"] == 0:
+            print(bcolors.FAIL + "\n"  + "You do not have anymore", item.name + "(s) .", "\n" + bcolors.ENDC)
+            continue
+
+        #Reduces quantity of item used
+        player1.items[item_choice]["Quantity"] -= 1
+
+        if item_choice == -1:
+            continue
+
+        if item.type == "Potion":
+            player1.heal(item.prop)
+            print(bcolors.OKGREEN + "\n" + item.name + " heals you for ", str(item.prop), "HP" + bcolors.ENDC)
+        elif item.type == "Elixir":
+            player1.hp = player1.max_hp
+            player1.mp = player1.max_mp
+            print(bcolors.OKGREEN + "\n" + item.name + " fully restores your HP/MP." + bcolors.ENDC)
+        elif item.type == "Throwing Star":
+            enemy.take_damage(item.prop)
+            print(bcolors.OKBLUE + "\n" + "You used "+ item.name + " it deals", str(item.prop), "points of damage." + bcolors.ENDC)
+    elif index!=0 and index!=1 and index!=2:
         print("\n" + bcolors.FAIL + "You seemed to have entered a bad number :(. You entered:", choice + ".",
-              "Your choices are '1' or '2'."+ bcolors.ENDC)
+              "Your choices are '1', '2', or '3'."+ bcolors.ENDC)
         continue
-
-
-
 
     enemy_choice = 1
 
@@ -87,7 +127,7 @@ while running:
     if enemy.get_hp() == 0:
         print(bcolors.OKGREEN + "THE ENEMY HAS BEEN SLAIN!" + bcolors.ENDC)
         running = False
-    elif player1.get_hp()  == 0:
+    elif player1.get_hp() == 0:
         print(bcolors.FAIL + "YOU HAVE BEEN SLAIN" + bcolors.ENDC)
         running = False
     elif enemy.get_hp() == 0 and player1.get_hp() == 0:
